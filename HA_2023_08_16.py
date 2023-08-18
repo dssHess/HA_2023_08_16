7d904f0edd2594527ad2e3da44406c375f43d869
dssHess <115979452+dssHess@users.noreply.github.com>
2023-08-18 10:40:10 +0200

    first commit

---
+++ HA_2023_08_16.py
@@ -0,0 +1,24 @@
+"""
+HA - 16.08.2023
+Aufgaben:
+•	Erstellt ein neues Python Projekt mit einem einfachen Skript (z. B. einer Funktion, die zwei Zahlen multipliziert oder einen String zu UPPERCASE macht). 
+•	Testet den Code in einem separaten Skript.
+•	Erstellt ein öffentliches GitHub Repository und pushed euren Code da rein
+•	Erstellt eine GitHub Action und lasst den Code automatisiert testen (Flake8 + Pytest)
+•	Lest das Kapitel: "Schnellstart" und "Informationen zu GitHub Actions" der Dokumentation (https://docs.github.com/de/actions/quickstart)
+Hilfestellungen:
+
+Schaut gerne in den Code rein, den wir heute morgen zusammen erstellt haben. Außerdem ist die GitHub Actions Doku sehr wertvoll. 
+
+Ihr könnt die Aufgaben abgeben, indem folgendes hochlädt:
+•	URL zum GitHub Repository
+
+Abgabetermin: 17. Aug.
+Details ansehen
+
+Gepostet am 2:00 PM, Aug. 16 (MESZ) von Djordje Atlialp
+
+"""
+def multi(za: int, zb: int) -> int:
+    return za * zb
+
---
+++ HA_2023_08_16_Test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from HA_2023_08_16 import multi 
+
+class TestMulti(unittest.TestCase):
+
+    def test_multi_function(self):
+        self.assertEqual(multi(1, 2), 2)
+        self.assertEqual(multi(4, 7), 28)
+
+    def test_multi_function_with_floats(self):
+        self.assertAlmostEqual(multi(3.1, 2.1), 6.51)
+
+
+if __name__ == "__main__":
+    unittest.main()
